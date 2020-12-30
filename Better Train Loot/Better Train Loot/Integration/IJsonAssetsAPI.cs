a9100b7e155c1d82e2d5b0b63c1aa13c51a0852e
Aair the Great <a_aair@hotmail.com>
2020-12-29 16:43:55 -0800

    Updated nuget package and getting ready for SDV 1.5

---
+++ Better Train Loot/Better Train Loot/Integration/IJsonAssetsAPI.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterTrainLoot.Interfaces
+{
+    public interface IJsonAssetsAPI
+    {
+        IDictionary<string, int> GetAllCropIds();
+        IDictionary<string, int> GetAllFruitTreeIds();
+    }
+}
